aea3178be618de319bb1fbe951a7a28d7dc09c80
Дмитрий <@MSI>
2019-11-06 00:41:07 +0700

    это из урока буду доделывать извеняюсь что не все завтра все постораюсь доделать (день почти свободен)

--- ArrayLesson/Array.cs
+++ ArrayLesson/Array.cs
@@ -4,23 +4,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayLesson
+namespace ConsoleApp5
 {
-    public class Array
+    class ArrayProg
     {
+        public int Size { get; private set; }
         private int[] _array;
-        public int Length { get; private set; }
-        public Array()
+        public ArrayProg(int[] array)
         {
-            Length = 0;
-            _array = new int[0];
-        }
-        public Array(int [] array)
-        {
-            Length = array.Length;
-            _array = new int[array.Length];
+            if (array == null) throw new ArgumentNullException("Array can not be null");
+            Size = array.Length;
+            array = new int[array.Length];
             for (int i = 0; i < array.Length; i++)
+            {
                 _array[i] = array[i];
+            }
+        }
+        public ArrayProg()
+        {
+            _array = new int[0];
+            Size = 0;
+        }
+        public int Summ()
+        {
+            int s = 0;
+            foreach (int el in _array)
+            {
+                s += el;
+
+            }
+            return s;
+        }
+        public int Mult()
+        {
+            int p = 1;
+            foreach (int el in _array)
+            {
+                p = el;
+            }
+            return p;
+        }
+        public int Max()
+        {
+            int max = _array[0];
+            foreach (int el in _array)
+            {
+                max = el > max ? el : max;
+            }
+            return max;
+        }
+        public int Min()
+        {
+            int min = _array[0];
+            foreach (int el in _array)
+            {
+                min = el < min ? el : min;
+            }
+            return min;
+        }
+        public void Print()
+        {
+            foreach (int el in _array)
+            {
+                Console.Write(el + " ");
+            }
+        }
+        public int Find(int val)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == val)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool HasValue(int val)
+        {
+            foreach (int el in _array)
+            {
+                if (el == val)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayProg arrays = new ArrayProg();
+
         }
     }
 }
