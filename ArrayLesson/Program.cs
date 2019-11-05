6175e95c2a8055f9d8fd3d5ad215311e276a71cd
Дмитрий <@MSI>
2019-11-06 00:44:05 +0700

    программа из урока извеняюсь что не сделал завтра буду доделывать (почти весь свободен)

--- ArrayLesson/Program.cs
+++ ArrayLesson/Program.cs
@@ -4,16 +4,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayLesson
+namespace ConsoleApp5
 {
+    class ArrayProg
+    {
+        public int Size { get; private set; }
+        private int[] _array;
+        public ArrayProg(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("Array can not be null");
+            Size = array.Length;
+            array = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                _array[i] = array[i];
+            }
+        }
+        public ArrayProg()
+        {
+            _array = new int[0];
+            Size = 0;
+        }
+        public int Summ()
+        {
+            int s = 0;
+            foreach (int el in _array)
+            {
+                s += el;
+
+            }
+            return s;
+        }
+        public int Mult()
+        {
+            int p = 1;
+            foreach (int el in _array)
+            {
+                p = el;
+            }
+            return p;
+        }
+        public int Max()
+        {
+            int max = _array[0];
+            foreach (int el in _array)
+            {
+                max = el > max ? el : max;
+            }
+            return max;
+        }
+        public int Min()
+        {
+            int min = _array[0];
+            foreach (int el in _array)
+            {
+                min = el < min ? el : min;
+            }
+            return min;
+        }
+        public void Print()
+        {
+            foreach (int el in _array)
+            {
+                Console.Write(el + " ");
+            }
+        }
+        public int Find(int val)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == val)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool HasValue(int val)
+        {
+            foreach (int el in _array)
+            {
+                if (el == val)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            Array array = new Array(arr);
-            Console.Write(" ");
-            Console.ReadKey();
+            ArrayProg arrays = new ArrayProg();
+
         }
     }
 }
